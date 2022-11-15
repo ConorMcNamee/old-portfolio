b2a97b0264cb90b891067cf1773688e1f63fe0d6
Conor <cmcnamee4@gmail.com>
2022-11-15 18:32:10 +0000

    Updated About section

--- components/Index/About.tsx
+++ components/Index/About.tsx
@@ -15,22 +15,20 @@ export default function  About() {
 
                     <p className="">
                         I&#39;ve been a developer for 4 years with 1 year experience in Industry 
-                        as well as a Freelancer for 4 months and a programmer for 9 years and 
-                        I am also a Final Year Computer Science Student
+                        as well as a Freelancer for 4 months and a programmer for 9 years. I am also a Final Year Computer Science Student
                     </p>
 
                     <br />
-                    <p>I have a very good understanding of the basics such as HTML, CSS and Javascript and I 
-                        also have a good understanding of basic web design principles
-                        I have a strong understand of backend technologies such as REST API`&#39;s and GraphQL
-
-                        I am proficient in SQL and have worked with PostgreSQL and MySQL
+                    <p>I have a very good understanding of the basics such as HTML, CSS and Javascript as well
+                        as basic web design principles. I have worked with many backend technologies and I am currently
+                        learning GraphQL. On top of this I also know SQL and have use PostgreSQL as well as MySQL. I also has solid grasp
+                        on Docker and Cloud technologies. I have all the qualities of a good Full Stack Developer
                     </p>
 
                     <br />
 
                     <p>
-                        I also understand SEO and I am more than capable of handling your SEO strategy. If you would like to discuss 
+                        I also SEO and I am more than capable of handling your SEO strategy. If you would like to discuss 
                         this in greater detail get <Link className="text-[#517EE1]" href="/contact">in contact</Link>
                     </p>
                 </div>
