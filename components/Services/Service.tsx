0b6bc7614d3db8676409765cbd4687bf074af2f8
cmcnamee25 <cmcnamee4@gmail.com>
2022-11-27 15:14:10 +0000

    Resolved Build errors

--- components/Services/Service.tsx
+++ components/Services/Service.tsx
@@ -18,7 +18,7 @@ export default function Service () {
             <div className="flex text-center items-center justify-center self-center bg-theme dark:bg-theme-dark drop-shadow-3xl m-5 p-10 w-[90%] lg:w-[50%]">
                 <div id="card" className="flex flex-col justify-center items-center">
                 <p className="text-4xl">Custom Web App and Software Development</p>
-                <p className="text-lg">Do you have an out of date software system or no system at all? I can develop anything from CRM's to Marketing Automation tools. Get in touch an we can talk about your needs and potential solutions</p>
+                <p className="text-lg">Do you have an out of date software system or no system at all? I can develop anything from CRM&#39;s to Marketing Automation tools. Get in touch an we can talk about your needs and potential solutions</p>
 
                 <Link className="text-theme-link text-xl hover:underline" href="/contact">Get in Touch</Link>
                 </div>
