b60af5ba86f578da92a1ee30fb58872cfdd7cd3a
40298979 <cmcnamee25@qub.ac.uk>
2023-01-02 03:20:21 +0000

    Updated Portfolio Section

--- components/Portfolio/PortfolioCard.tsx
+++ components/Portfolio/PortfolioCard.tsx
@@ -13,20 +13,22 @@ const PortfolioProjects = [
       "link": "https://widgets.potion.so/widgets/pomodoro",
     },
     {
+      "title": "JosiahEPhotography",
+      "desc": "I worked with this client to design and develop a website using Webflow and now I am managing his SEO for him",
+      "img": ""
+    },
+    {
       "title": "ByteLoop",
       "desc": "This is a Open Source Project that I am working on with the Owner Nick. First contribution to the project has been an SQL Schema. As there is no frontend I'm not able to include any images however, the github is linked below",
       "link": "https://github.com/nickpickcodes/ByteLoop.Database"
     },
+
     {
       "title": "Web Design Examples",
       "desc": "Here are some examples of my web design. I was never blessed with the ability of design however, through study and practice of web design techiques I became good. All of the websites I design are designed for two things. Professionalism and Conversion",
-      "link": "http://localhost:3000/portfolio/web-design"
+      "link": "https://conormcnamee.dev/portfolio/web-design"
     },
-    // {
-    //   "title": "Go, GraphQL Todo List",
-    //   "desc": "This is a client I have been working with to develop a pomodoro timer widget that embeds into Notion",
-    //   "img": "/potion-pomodoro.png"
-    // },
+
 ]
 
 export default function PortfolioCard() {
