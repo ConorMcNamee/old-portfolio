e9f5d2bfbb8f767ae68de9bb2ca3cdb8af648799
40298979 <cmcnamee25@qub.ac.uk>
2023-05-04 13:08:50 +0100

    Updated the about me section

--- components/Index/About.tsx
+++ components/Index/About.tsx
@@ -7,27 +7,39 @@ export default function  About() {
             <div className='flex flex-col text-center md:w-[50%] mt-10'>
                 <h3 className="text-4xl m-5">About</h3>
                 <hr className="border-2 opacity-25 w-[50%] self-center" />
-                <div className="text-xl m-5 md:text-3xl">
+                <div className="text-xl m-5">
                     
-                    <p className="">
+                    <p>
                         20 Year old Full Stack Developer from Belfast, U.K
                     </p>
 
                     <br />
 
-                    <p className="">
-                        Developer for 9 Years. 1 Year Industry, 5 Months as a Freelancer
+                    <p>
+                    I started learning how to program when I was 11. Making simple HTML, CSS and Javascript web pages‍
                     </p>
 
                     <br />
 
-                    <p>Look below to see the different skills I can provide to help your business grow</p>
+                    <p>From there I started to learn Java and C++ but never really got anywhere with those languages
+                        I then started to experiment with many other languages like Ruby, SQL, Python and Even Haskell</p>
 
                     <br />
 
                     <p>
-                        Also capable of managing or creating your SEO Strategy If you would like to discuss 
-                        this in greater detail get <Link className="text-theme-highlight" href="/contact">in contact</Link>
+                        Once I got nowhere with any of those languages around 2019 I began to focus on just web development
+                        learning React, Django and then Flask and different databases like MySQL, Postgres
+                    </p>
+                    <br />
+                    <p>
+                        And over the past year I've been really beginning to become an expert on Cloud services like AWS and Google Cloud Platform
+                    </p>
+
+                    <br />
+
+                    <p>
+                        
+                        <Link className="text-theme-highlight" href="/contact">Get in touch</Link>
                     </p>
                 </div>
             </div>
