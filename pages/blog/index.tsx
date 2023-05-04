6f2242eb6d679344bcb8275cd27243a2e6faf69e
40298979 <cmcnamee25@qub.ac.uk>
2023-05-04 14:08:51 +0100

    Fixed blog slug page and updated blog posts logic

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import Logos from "../../components/Layout/Logos";
+import { getSortedPostsData } from "../../lib/posts";
+
+import ListItem from "../../components/Blog/ListItem";
+
 
 export default function Blog() {
+    const posts = getSortedPostsData()
     return (
         <section className="h-screen w-screen flex justify-center text-center items-center">
-            <div className="flex flex-col text-xl">
-                <h1>This page is unable right now!</h1>
-                <h1>Come back later!</h1>
-                <Link className="text-[#517EE1] hover:underline" href="/">Go home</Link>
-            </div>
-            <Logos></Logos>
+            {posts.map(post => (
+                    <ListItem key={post.id} post={post} />
+                ))}
         </section>
     );
 }
